20decc23e8b59d0add85ede6a5a53ed13a51c557
Tabita99 <tabita.m@hotmail.co.uk>
2020-10-16 15:41:57 +0000

    sign-up function for signup html page created

--- app.py
+++ app.py
@@ -23,6 +23,10 @@ def home_page():
     return render_template("home.html")
 
 
+@app.route("/sign_up", methods=["GET", "POST"])
+def sign_up():
+    return render_template("/signup.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
