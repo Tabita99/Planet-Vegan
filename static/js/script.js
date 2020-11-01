060206a586570ed8cec83005ef52986fdde17780
Tabita99 <tabita.m@hotmail.co.uk>
2020-11-01 18:47:07 +0000

    category select drop down enabled

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 $( document ).ready(function(){
     $(".dropdown-trigger").dropdown();
     $('.sidenav').sidenav();
-     $(".dropdown2").dropdown();
+    $(".dropdown2").dropdown();
+    $('select').formSelect();
     
 });
