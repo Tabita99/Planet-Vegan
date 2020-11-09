91a99b79fbda0bb0e8d58fc126246d550ab0de4f
Tabita99 <tabita.m@hotmail.co.uk>
2020-11-09 16:13:32 +0000

    dinner/dessert/lunch/breakfast routes deleted

--- app.py
+++ app.py
@@ -92,28 +92,6 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/breakfast")
-def breakfast():
-    return render_template(
-        "breakfast.html", recipes=mongo.db.recipes.find(
-         {"category_name": ObjectId("5f7b3d02080238ed1ae00951")}
-        ))
-
-
-@app.route("/lunch")
-def lunch():
-    return render_template("lunch.html", recipes=mongo.db.recipes.find(
-         {"category_name": ObjectId("5f7b3d23080238ed1ae00952")}
-    ))
-
-
-@app.route("/dinner")
-def dinner():
-    return render_template("dinner.html", recipes=mongo.db.recipes.find(
-         {"category_name": ObjectId("5f7b3d59080238ed1ae00954")}
-    ))
-
-
 @app.route("/category/<category_id>", methods=["GET"])
 def category_recipes(category_id):
     category = mongo.db.category.find_one({"_id": ObjectId(category_id)})
@@ -121,13 +99,6 @@ def category_recipes(category_id):
     recipes = mongo.db.recipes.find({"category_name": ObjectId(category_id)})
     return render_template(
         "categories_recipes.html", recipes=recipes, category=category)
-
-
-@app.route("/dessert")
-def dessert():
-    return render_template("dessert.html", recipes=mongo.db.recipes.find(
-         {"category_name": ObjectId("5f7b3d83080238ed1ae00955")}
-    ))
 
 
 @app.route("/open_recipe /<recipe_id>")
